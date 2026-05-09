59b235d25dbf779b1fb4dafeb7e36d14f3107cb1
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-05-09 23:11:22 +0530

    Power Set

---
+++ Bitwise operator/Power-Set.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    static int xorTillN(int n) {
+        if (n % 4 == 0) return n;
+        if (n % 4 == 1) return 1;
+        if (n % 4 == 2) return n + 1;
+        return 0;
+    }
+
+    // XOR from L to R
+    static int findXOR(int L, int R) {
+        return xorTillN(R) ^ xorTillN(L - 1);
+    }
+
+    public static void main(String[] args) {
+        int L = 3, R = 5;
+
+        System.out.println(findXOR(L, R));
+    }
+}
