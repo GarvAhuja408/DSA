5b077ef1ecea50da47b6cb88d3a7450b51377ed3
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-05-13 23:05:37 +0530

    Power of two

---
+++ Bitwise operator/powerOfTwo.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    static boolean isPowerOfTwo(int n) {
+
+        if (n <= 0) {
+            return false;
+        }
+
+        return (n & (n - 1)) == 0;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 16;
+
+        System.out.println(isPowerOfTwo(n));
+    }
+}
