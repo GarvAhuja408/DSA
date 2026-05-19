e70278a5e42439b5fd07b02159766cb939575f1a
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-05-19 22:18:07 +0530

    Count number of bits to be flipped to convert A to B

---
+++ Bitwise operator/minBitFlips.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public static int minBitFlips(int start, int goal) {
+
+        int xor = start ^ goal;
+        int count = 0;
+
+        while(xor != 0) {
+
+            count += (xor & 1);
+            xor = xor >> 1;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int start = 10;
+        int goal = 7;
+
+        System.out.println(minBitFlips(start, goal));
+    }
+}
