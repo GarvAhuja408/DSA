99cc70da5371d047f794ae8e26a5a452f55ed0fb
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-05-20 19:06:15 +0530

    Pow(x,n)

---
+++ Recursion/Pow(x, n).java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public static double myPow(double x, int n) {
+
+        long N = n;
+
+        if(N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return power(x, N);
+    }
+
+    public static double power(double x, long n) {
+
+        if(n == 0) {
+            return 1;
+        }
+
+        double half = power(x, n / 2);
+
+        if(n % 2 == 0) {
+            return half * half;
+        }
+
+        return x * half * half;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(myPow(2, 10));
+    }
+}
