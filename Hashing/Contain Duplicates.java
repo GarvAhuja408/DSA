a1b5130348b51299e868ba250842081f617c6584
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-06-15 15:56:51 +0530

    Contain duplicates

---
+++ Hashing/Contain Duplicates.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums));
+    }
+}
