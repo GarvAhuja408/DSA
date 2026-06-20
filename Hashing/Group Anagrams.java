55760daf99ca51c20300ebac73a34b78fb40a184
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-06-20 12:26:55 +0530

    Group anagrams

--- Hashing/Group Anagrams.java
+++ Hashing/Group Anagrams.java
@@ -1,23 +1,22 @@
 import java.util.*;
-
-public class Main {
-    public static List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<>();
-
-        for (String s : strs) {
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String key = new String(arr);
-
-            map.putIfAbsent(key, new ArrayList<>());
-            map.get(key).add(s);
+class Main {
+    public static List<List<String>> groupAnagram(String strs[]){
+        HashMap< String, List<String> > map = new HashMap<>();
+        
+        for(String word : strs){
+            char chars[] = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            
+            if(!map.containsKey(key)){
+                map.put( key,new ArrayList<>() );
+            }
+            map.get(key).add(word);
         }
-
         return new ArrayList<>(map.values());
     }
-
     public static void main(String[] args) {
-        String[] strs = {"eat","tea","tan","ate","nat","bat"};
-        System.out.println(groupAnagrams(strs));
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.print(groupAnagram(strs));
     }
 }
