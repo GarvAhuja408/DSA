942d4f06347813f038bb44010345b449ff22838d
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-06-21 12:39:29 +0530

    Left rotate an array by K places

---
+++ Arrays/Left Rotate Array by K Places.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    static void rotateByK(int[] arr, int k) {
+        int n = arr.length;
+        k %= n;
+
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+
+    static void reverse(int[] arr, int left, int right) {
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 2;
+
+        rotateByK(arr, k);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
