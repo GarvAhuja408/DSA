bc9875678e97189e0caf9eeeff6267d8bfd37b16
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-07-01 23:02:06 +0530

    Set matrix zeros

---
+++ Arrays/Set Matrix Zeroes.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+class Main {
+
+    public static void setZeroes(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        for (int j = 0; j < cols; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                fillRow(matrix, i);
+            }
+        }
+
+        for (int j = 1; j < cols; j++) {
+            if (matrix[0][j] == 0) {
+                fillCol(matrix, j);
+            }
+        }
+
+        if (firstRowZero) {
+            fillRow(matrix, 0);
+        }
+
+        if (firstColZero) {
+            fillCol(matrix, 0);
+        }
+    }
+
+    public static void fillRow(int[][] matrix, int row) {
+        for (int j = 0; j < matrix[0].length; j++) {
+            matrix[row][j] = 0;
+        }
+    }
+
+    public static void fillCol(int[][] matrix, int col) {
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][col] = 0;
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+        };
+
+        setZeroes(matrix);
+        printMatrix(matrix);
+    }
+}
