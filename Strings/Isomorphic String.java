a6c7199db79a5c27dd77834829fa5220e3668e0e
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-07-07 11:07:05 +0530

    Isomorphic String

---
+++ Strings/Isomorphic String.java
@@ -0,0 +1,35 @@
+class Main {
+
+    static boolean isIsomorphic(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] mapST = new char[256];
+        char[] mapTS = new char[256];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if (mapST[c1] == 0 && mapTS[c2] == 0) {
+                mapST[c1] = c2;
+                mapTS[c2] = c1;
+            } else if (mapST[c1] != c2 || mapTS[c2] != c1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "egg";
+        String t = "add";
+
+        System.out.println(isIsomorphic(s, t));
+    }
+}
