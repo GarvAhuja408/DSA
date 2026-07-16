1aa616c67f30fa610bad5f26a224ce5128719d4d
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-07-16 12:10:33 +0530

    Search Single Element in a sorted array

---
+++ BinarySearch/Search Single Element in a sorted array.java
@@ -0,0 +1,39 @@
+class Main {
+    public static int searchSingle(int arr[]){
+        int n =arr.length;
+        
+        if(n==1){
+            return arr[0];
+        }
+        
+        if(arr[0]!=arr[1]){
+            return arr[0];
+        }
+        
+        if(arr[n-1]!=arr[n-2]){
+            return arr[n-1];
+        }
+        
+        int start = 0; 
+        int end = n-1;
+        
+        while(start<=end){
+            
+            int mid = start+(end-start)/2;
+            
+            if(arr[mid]!=arr[mid-1]&&arr[mid]!=arr[mid+1]){
+                return arr[mid];
+            }
+            else if( (mid % 2 ==1 && arr[mid]==arr[mid-1]) ||  (mid % 2 ==0 && arr[mid]==arr[mid+1])){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,1,2,2,3,3,5,6,6};
+        System.out.print(searchSingle(arr));
+    }
+}
