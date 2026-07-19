36ac8ecac2abe7e942f22a81c6c2407ae985b7dc
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-07-19 12:43:55 +0530

    Next Greate Element

---
+++ Stack/Next Greater Element.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Main {
+    static int[] nextGreaterElement(int[] arr) {
+
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+
+        return ans;
+    }
+
+    static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {4, 5, 2, 10, 8};
+
+        int[] ans = nextGreaterElement(arr);
+
+        print(ans);
+    }
+}
