fd65b18404486a3cf5bbd7b2c30461a6cd6ed5d3
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-02 23:19:45 +0530

    Length of loop in LL

---
+++ LinkedList/Length of Loop in Linked List.java
@@ -0,0 +1,48 @@
+class Main {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static int lengthOfLoop(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                int length = 1;
+                fast = fast.next;
+
+                while (slow != fast) {
+                    length++;
+                    fast = fast.next;
+                }
+
+                return length;
+            }
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        head.next.next.next.next.next = head.next;
+
+        System.out.println(lengthOfLoop(head));
+    }
+}
